c5293950fd8f3c66ff1b71b3dd67031087dc10c7
unknown <yosemitebone@gmail.com>
2026-04-01 11:40:16 -0700

    Added submit button to character entry form

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -2,17 +2,20 @@ import React, { useState } from "react";
 
 
 
-function Form() {
+function Form(props) {
     const [person, setPerson] = useState({
         name: "",
         job: ""
     })
     function handleChange(event) {
-    const { name, value } = event.target;
-    if (name === "job")
-        setPerson({ name: person["name"], job: value });
-    else setPerson({ name: value, job: person["job"] });
-    
+        const { name, value } = event.target;
+        if (name === "job")
+            setPerson({ name: person["name"], job: value });
+        else setPerson({ name: value, job: person["job"] });      
+    }
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({ name: "", job: ""});
     }
     return (
         <form>
@@ -32,6 +35,7 @@ function Form() {
                 value={person.job}
                 onChange={handleChange}
             />
+            <input type="button" value="Submit" onClick={submitForm} />
         </form>
     );
 }
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -23,20 +23,23 @@ const characters = [
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
-
+    function updateList(person) {
+        setCharacters([...characters, person]);
+    }
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
             return i != index;
         });
         setCharacters(updated);
     }
+
     return (
     <div className="container">
         <Table 
             characterData={characters}
             removeCharacter={removeOneCharacter} 
         />
-        <Form />
+        <Form handleSubmit={updateList} />
     </div>
     );
 }
