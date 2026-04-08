66c61a4ec8fce3f7554fd0ee2266b0204efd15ba
unknown <yosemitebone@gmail.com>
2026-04-08 10:20:34 -0700

    Added users pages by name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -37,12 +37,27 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("/users", (req, res) => {
-  res.send(users);
-});
+
 
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
+});
+
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name
+    );
+};
+
+app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    } else {
+        res.send(users);
+    }
 });
